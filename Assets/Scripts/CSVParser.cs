a680485361e9150f3df8e4a6f6b8429b03de637c
mpbastecki <mpbastecki@gmail.com>
2023-02-01 16:36:56 -0500

    uploading unity project to github

---
+++ Assets/Scripts/CSVParser.cs
@@ -0,0 +1,226 @@
+﻿/*
+ *  @class      CSVParser
+ *  @purpose    Request card information from locally-stored CSV files and populate card list
+ *  
+ *  @author     John Georgvich
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class CSVParser
+{
+    //  string declaration block
+    //      strings are required; class variables hold project referenced to csv datafiles
+    private string actionPath, requirementPath, deckOnePath, deckTwoPath, deckThreePath, deckFourPath; 
+    
+    /*
+     *  @name       LoadFiles()
+     *  @purpose    Sets appropriate directory locations for CSV datafiles
+     *                  Separate method for a reason; if CSV locations are altered/new CSVs added, change here
+     */
+    public void LoadFiles()
+    {
+        //  @note:  root directory is project directory, as per Unity
+        this.actionPath = "Assets/Resources/Data/Actions.csv";
+        this.requirementPath = "Assets/Resources/Data/Requirements.csv";
+        this.deckOnePath = "Assets/Resources/Data/DeckOne.csv";
+        this.deckTwoPath = "Assets/Resources/Data/DeckTwo.csv";
+        this.deckThreePath = "Assets/Resources/Data/DeckThree.csv";
+        this.deckFourPath = "Assets/Resources/Data/DeckFour.csv";
+    }
+
+    /*
+     *  @name       ParseRequirements()
+     *  @purpose    Parses Requirements.csv; returns List<string[]> of card requirements for use by GameManager obj.
+     *                  List<string[]> *is* necessary because requirements (requirement ID & card ID) are stored in pairs
+     *                      Each entry in the list will be as follows:
+     *                          Index 0: requirement ID
+     *                          Index 1: card ID
+     *                  Returned List<string[]> is also used to populate requirements in Deck construction
+     *                  See below:  GetDecks()
+     *  
+     *  @return     List<string[]> containing requirement refs.
+     */
+    public List<string[]> ParseRequirements()
+    {
+        List<string[]> requirementIDs = new List<string[]>();
+        using (var parser = new StreamReader(this.requirementPath))
+        {
+            while (!parser.EndOfStream)
+            {
+                var line = parser.ReadLine();
+                string[] requirementCard = line.Split(',');
+                requirementIDs.Add(requirementCard);
+            }
+        }
+
+        return requirementIDs;
+    }
+
+    /*
+     *  @name       ParseActionIDs()
+     *  @purpose    Parses Actions.csv; returns string[] of card actions for use by GameManager obj.
+     *                  List<string[]> *is* necessary because requirements (action ID & card ID) are stored in pairs
+     *                      Each entry in the list will be as follows:
+     *                          Index 0: action ID
+     *                          Index 1: card ID
+     *                  
+     *                  Returned string[] is also used to populate actionIDs in Deck construction
+     *                  See below:  GetDecks()
+     *                  
+     *  @return     List<string[]> containing actionID refs.
+     */
+    public List<string[]> ParseActionIDs()
+    {
+        List<string[]> actionIDs = new List<string[]>();
+        using (var parser = new StreamReader(this.actionPath))
+        {
+            while (!parser.EndOfStream)
+            {
+                var line = parser.ReadLine();
+                string[] actionCard = line.Split(',');
+                actionIDs.Add(actionCard);
+            }
+        }
+
+        return actionIDs;
+    }
+
+    /*
+     *  @name       DeckParse()
+     *  @param      string deckPath                 filepath of the deck-specific CSV to be parsed
+     *              List<string[]> actionList       list of action IDs to populate card-specific actions
+     *              List<string[]> requirementList  list of requirement IDs to populate card-specific requirements
+     *              
+     *  @purpose    Builds individual, specified deck
+     *                  Populates a single deck with cards and returns
+     *  @return     Deck object
+     */
+     
+    private Deck DeckParse(string deckPath, List<string[]> actionList, List<string[]> requirementList)
+    {
+        //  temporary Deck obj. for return
+        Deck parsedDeck = new Deck();
+
+        using (var parser = new StreamReader(deckPath))
+        {
+            //  reads single Deck from specified filepath (@param deckpath), adds parsed cards to temporary Deck obj
+            while (!parser.EndOfStream)
+            {
+                var line = parser.ReadLine();
+                /*  
+                 *  calls Card constructor with parameter of line.Split(',')
+                 *      line.Split splits a line of the document into an array
+                 *      see Card overloaded constructor for contents of array and indices
+                 */
+                Card tempCard = new Card(line.Split(','));
+                parsedDeck.Cards.Add(tempCard);
+            }
+        }
+
+        /*
+         *  populates actionIDs and requirementIDs for each card in temporary deck obj.
+         *      action/requirement arrays are searched (two loops because array-size is drastically different)
+         *      if cardID from array matches cardID from the foreach-specified Card object in the temporary Deck obj, 
+         *          the associated action/requirement ID is added to the foreach-specified Card objects' appropriate List
+         *              See Card class for List
+         */
+        foreach(Card card in parsedDeck.Cards)
+        {
+            for(int i = 0; i < actionList.Count; i++)
+            {
+                string[] actionArray = actionList[i];
+                string actionID = actionArray[0];
+                string cardID = actionArray[1];
+
+                if(card.CardID == cardID)
+                {
+                    card.ActionID.Add(actionID);
+                }
+            }
+            for(int i = 0; i < requirementList.Count; i++)
+            {
+                string[] requirementArray = requirementList[i];
+                string requirementID = requirementArray[0];
+                string cardID = requirementArray[1];
+
+                if(card.CardID == cardID)
+                {
+                    card.ReqID.Add(requirementID);
+                }
+            }
+        }
+
+        //  temporary deck object is returned
+        return parsedDeck;
+    }
+
+
+    /*
+     *  @name       GetDecks()
+     *  @param      List<string[]> actionList       list of action IDs to populate card-specific actions
+     *              List<string[]> requirementList  list of requirement IDs to populate card-specific requirements
+     *              
+     *  @purpose    Parses each deck-specific CSV to create a permanent Deck object, 
+     *                  populates Deck object data-members
+     *  @return     List<Deck> containing all decks parsed
+     */
+    public List<Deck> GetDecks(List<string[]> actionList, List<string[]> requirementList)
+    {
+        //  temporary list of decks; to be returned
+        List<Deck> parsedDecks = new List<Deck>();
+        //  array is used to parse-by-loop; see below
+        string[] deckPaths = new string[] { this.deckOnePath, this.deckTwoPath, this.deckThreePath, this.deckFourPath };
+
+        //  used for deck ID numbering; see below
+        int deckCount = 0;
+
+        /*
+         *  MAIN LOOP:
+         *      deckCount is incremented (starting value is 0; value is 1 on first deck parse)
+         *      temporary Deck object is created from specified CSV (deckPaths)
+         *      Deck.DeckID is assigned via string concatenation w/ int deckCount
+         *          value is 1 on first deck parse, DeckID will be "D00" + "1" = "D001"
+         *      Based on DeckID value, Deck.DeckName and Deck.DeckColor are assigned
+         *          these are hard-coded, predetermined values
+         *      Parsed and complete deck is added to temporary list of decks for return
+         */
+        foreach (string deckPath in deckPaths)
+        {
+            deckCount++;
+            Deck tempDeck = DeckParse(deckPath, actionList, requirementList);
+            tempDeck.DeckId = "D00" + deckCount;
+            switch (tempDeck.DeckId)
+            {
+                case ("D001"):
+                    tempDeck.DeckName = "Allegheny Forest";
+                    tempDeck.DeckColor = new Color32(58, 102, 44, 128);
+                    break;
+                case ("D002"):
+                    tempDeck.DeckName = "Appalachian Homestead";
+                    tempDeck.DeckColor = new Color32(166, 135, 82, 128);
+                    break;
+                case ("D003"):
+                    tempDeck.DeckName = "Peat Bogs";
+                    tempDeck.DeckColor = new Color32(124, 56, 58, 128);
+                    break;
+                case ("D004"):
+                    tempDeck.DeckName = "Clarion River";
+                    tempDeck.DeckColor = new Color32(116, 126, 140, 128);
+                    break;
+                case (null):
+                    Debug.Log("You shouldn't have come here (CSVParser.GetDecks()).");
+                    //  null case is debug only; you shouldn't reach this position
+                    break;
+            }
+            //  add temporary deck to list of temporary deck objects
+            parsedDecks.Add(tempDeck);
+        }
+
+        //  return temporary list of deck objects; see GameManager
+        return parsedDecks;
+    }
+}
---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,207 @@
+﻿/*
+ *  @class      Card.cs
+ *  @purpose    Store card information for use by internal game objects
+ *  
+ *  @author     John Georgvich
+ *  @date       2020
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card {
+
+    //  class variable declaration block
+    private string cardID;
+    private string cardName;
+    private string cardType;
+    private int pointValue;
+    private string kingdom;
+    private string subkingdom;
+    private string superdivision;
+    private string division;
+    private string subdivision;
+    private string superphylum;
+    private string phylum;
+    private string subphylum;
+    private string cardClass;
+    private string superclass;
+    private string subclass;
+    private string superorder;
+    private string order;
+    private string suborder;
+    private string superfamily;
+    private string family;
+    private string subfamily;
+    private string supergenus;
+    private string genus;
+    private string subgenus;
+    private string superspecies;
+    private string species;
+    private string subspecies;
+    private string animalSize;
+    private string animalEnvironment;
+    private string animalDiet;
+    private string plantType;
+    private string regionType;
+    private string domain;
+    private string infraclass;
+    private string infraorder;
+    private string section;
+    private string tribe;
+    private string fungi_type;
+    private string standingAction;
+    private string specialAction;
+    private string cardNotes;
+
+    public List<string> reqID;
+    private List<string> actionID;
+
+    //  Accessor and mutator block
+    public string CardID { get { return cardID; } set { cardID = value; } }
+    public string CardName { get { return cardName; } set { cardName = value; } }
+    public string CardType { get { return cardType; } set { cardType = value; } }
+    public int PointValue { get { return pointValue; } set { pointValue = value; } }
+    public string Kingdom { get { return kingdom; } set { kingdom = value; } }
+    public string Subkingdom { get { return subkingdom; } set { subkingdom = value; } }
+    public string Superdivision { get { return superdivision; } set { superdivision = value; } }
+    public string Division { get { return division; } set { division = value; } }
+    public string Subdivision { get { return subdivision; } set { subdivision = value; } }
+    public string Superphylum { get { return superphylum; } set { superphylum = value; } }
+    public string Phylum { get { return phylum; } set { phylum = value; } }
+    public string Subphylum { get { return subphylum; } set { subphylum = value; } }
+    public string CardClass { get { return cardClass; } set { cardClass = value; } }
+    public string Superclass { get { return superclass; } set { superclass = value; } }
+    public string Subclass { get { return subclass; } set { subclass = value; } }
+    public string Superorder { get { return superorder; } set { superorder = value; } }
+    public string Order { get { return order; } set { order = value; } }
+    public string Suborder { get { return suborder; } set { suborder = value; } }
+    public string Superfamily { get { return superfamily; } set { superfamily = value; } }
+    public string Family { get { return family; } set { family = value; } }
+    public string Subfamily { get { return subfamily; } set { subfamily = value; } }
+    public string Supergenus { get { return supergenus; } set { supergenus = value; } }
+    public string Genus { get { return genus; } set { genus = value; } }
+    public string Subgenus { get { return subgenus; } set { subgenus = value; } }
+    public string Superspecies { get { return superspecies; } set { superspecies = value; } }
+    public string Species { get { return species; } set { species = value; } }
+    public string Subspecies { get { return subspecies; } set { subspecies = value; } }
+    public string AnimalSize { get { return animalSize; } set { animalSize = value; } }
+    public string AnimalEnvironment { get { return animalEnvironment; } set { animalEnvironment = value; } }
+    public string AnimalDiet { get { return animalDiet; } set { animalDiet = value; } }
+    public string PlantType { get { return plantType; } set { plantType = value; } }
+    public string RegionType { get { return regionType; } set { regionType = value; } }
+    public string Domain { get { return domain; } set { domain = value; } }
+    public string Infraclass { get { return infraclass; } set { infraclass = value; } }
+    public string Infraorder { get { return infraorder; } set { infraorder = value; } }
+    public string Section { get { return section; } set { section = value; } }
+    public string Tribe { get { return tribe; } set { tribe = value; } }
+    public string Fungi_type { get { return fungi_type; } set { fungi_type = value; } }
+    public List<string> ReqID { get { return reqID; } set { reqID = value; } }
+    public List<string> ActionID { get { return actionID; } set { actionID = value; } }
+    public string StandingAction { get { return this.standingAction; } set { this.standingAction = value; } }
+    public string SpecialAction { get { return this.specialAction; } set { this.specialAction = value; } }
+    public string CardNotes { get { return this.cardNotes; } set { this.cardNotes = value; } }
+
+    /*
+     *  @name       Card(string[] values)
+     *  @author     John Georgvich
+     *  @purpose    Class constructor with parameters
+     *                  Builds class with string[] passed by CSVParser during deck build
+     *                  
+     *  @note       There *is* an index offset; values[7] is missing
+     */
+    public Card(string[] values)
+    {
+        this.cardID = values[0];
+        this.cardName = values[1];
+        this.cardType = values[2];
+        this.pointValue = int.Parse(values[3]);
+        this.kingdom = values[4];
+        this.subkingdom = values[5];
+        this.superphylum = values[6];
+        this.phylum = values[8];
+        this.subphylum = values[9];
+        this.superclass = values[10];
+        this.cardClass = values[11];
+        this.subclass = values[12];
+        this.superorder = values[13];
+        this.order = values[14];
+        this.suborder = values[15];
+        this.superfamily = values[16];
+        this.family = values[17];
+        this.subfamily = values[18];
+        this.supergenus = values[19];
+        this.genus = values[20];
+        this.subgenus = values[21];
+        this.superspecies = values[22];
+        this.species = values[23];
+        this.subspecies = values[24];
+        this.animalSize = values[25];
+        this.animalEnvironment = values[26];
+        this.animalDiet = values[27];
+        this.plantType = values[28];
+        this.regionType = values[29];
+        this.domain = values[30];
+        this.infraclass = values[31];
+        this.infraorder = values[32];
+        this.section = values[33];
+        this.tribe = values[34];
+        this.division = values[35];
+        this.superdivision = values[36];
+        this.subdivision = values[37];
+        this.fungi_type = values[38];
+        this.standingAction = values[39].ToString();
+        this.specialAction = values[40].ToString();
+        this.cardNotes = values[41];
+
+        this.reqID = new List<string>();
+        this.actionID = new List<string>();
+    }
+    
+    /*  
+     *  @name       Card()
+     *  @purpose    Default constructor for Card class
+     */
+    public Card()
+    {
+        CardID = "";
+        CardName = "";
+        CardType = "";
+        PointValue = 0;
+        Kingdom = "";
+        Subkingdom = "";
+        Superdivision = "";
+        Division = "";
+        Subdivision = "";
+        Superphylum = "";
+        Phylum = "";
+        Subphylum = "";
+        Superclass = "";
+        CardClass = "";
+        Subclass = "";
+        Superorder = "";
+        Order = "";
+        Suborder = "";
+        Superfamily = "";
+        Family = "";
+        Subfamily = "";
+        Supergenus = "";
+        Genus = "";
+        Subgenus = "";
+        Superspecies = "";
+        Species = "";
+        Subspecies = "";
+        AnimalSize = "";
+        AnimalEnvironment = "";
+        AnimalDiet = "";
+        PlantType = "";
+        RegionType = "";
+        StandingAction = "";
+        SpecialAction = "";
+        CardNotes = "";
+
+        reqID = new List<string>();
+        actionID = new List<string>();
+    }
+}
