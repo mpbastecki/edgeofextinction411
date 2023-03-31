12c3cb6e11a1c93da663f41ab18fc88686df92dc
XxFiveMsxX <116026590+XxFiveMsxX@users.noreply.github.com>
2023-03-31 14:16:46 -0400

    cardpickerpanal
    I made an overlay for a card picker panel

---
+++ Assets/Scripts/CardPickerPanelOpener.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardPickerPanelOpener : MonoBehaviour
+{
+    public GameObject CardPickerPanel;
+   public void OpenCardPickerPanel()
+    {
+        
+        if (CardPickerPanel != null)
+        {
+            CardPickerPanel.SetActive(true);
+        }
+    }
+
+    public void CloseCardPickerPanel()
+    {
+
+        if (CardPickerPanel != null)
+        {
+            CardPickerPanel.SetActive(false);
+        }
+    }
+
+}
