eaf166e11a2bd3d9029a95afc6b3a22fff56b330
XxFiveMsxX <116026590+XxFiveMsxX@users.noreply.github.com>
2023-04-12 15:21:12 -0400

    Action Button Script
    Made an action Button Script to get the action button to work

---
+++ Assets/Scripts/ActionButton.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionButton : Player
+{
+    //  private CardPickerPanelOpener currentPlayer;
+
+    //CurrentPlayer.this;
+    public GameObject CardInfoPanel;
+    public GameObject newObject;
+
+    public void CloseInfoPanal()
+    {
+        if (CardInfoPanel != null)
+        {
+            CardInfoPanel.SetActive(false);
+        }
+
+    }
+
+    public void logTestfunc()
+    {
+        int i = 2;
+        switch (i)
+        {
+            case 0:
+                TestFuncOne();
+                break;
+            case 1:
+                TestFuncTwo();
+                break;
+            case 2:
+                TestFuncThree();
+                break;
+        }
+
+
+
+    }
+
+
+    public void TestFuncOne()
+    {
+        Debug.Log("First test func worked");
+    }
+    public void TestFuncTwo()
+    {
+        Debug.Log("Second test func worked");
+    }
+    public void TestFuncThree()
+    {
+        Debug.Log("Third test func worked");
+    }
+
+
+    // public CardPickerPanelOpener CurrentPlayer { get => currentPlayer; set => currentPlayer = value; }
+}
