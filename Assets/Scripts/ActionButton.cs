703601286f08511fdad6092e642f6a2a111c1f0a
mpbastecki <mpbastecki@gmail.com>
2023-04-13 13:01:30 -0400

    merge from plants

---
+++ Assets/Scripts/ActionButton.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActionButton : Player
+{
+
+    private Text cardName;
+   
+
+    public GameObject CardInfoPanel;
+    public GameObject newObject;
+    
+    public void WhatActionIsHappening()
+    {
+        
+       
+        CardName = GameObject.Find("CardName").GetComponent<Text>();
+
+        int i = 2;
+        switch (CardName.text)
+        {
+            case ("Forest"):
+                TestFuncOne();
+                break;
+            case ("Extinction"):
+                TestFuncTwo();
+                break;
+            case (""):
+                TestFuncThree();
+                break;
+        }
+
+        Debug.Log(CardName.text);
+    }
+
+
+    public void TestFuncOne()
+    {
+        Debug.Log("First test func worked");
+    }
+
+    public void TestFuncTwo()
+    {
+        Debug.Log("Second test func worked");
+    }
+
+    public void TestFuncThree()
+    {
+        Debug.Log("Third test func worked");
+
+    }
+
+ 
+    public Text CardName { get => cardName; set => cardName = value; }
+
+}
---
+++ Assets/Scripts/PassiveAffects.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class passiveaffects : Requirements
+{
+    //private passiveaffects currentPlayer;
+    //private passiveaffects person = GameManager.Instance.Person;
+    
+    //private bool canDraw;
+    //private bool cardDiscarded;
+   
+
+  
+
+
+    //public override void InitializeObjects(string pScoreGameObject, string pRoundGameObject, string pHandGameObject, string pRegionGameObject, string pConditionGameObject,
+    //     string pPlantGameObject, string pInvertebrateGameObject, string pAnimalGameObject, string pSpecialRegionGameObject, string pMultiplayerGameObject,
+    //     string pMicrobeGameObject, string pFungiGameObject, string pDiscardGameObject, string pDeckColorGameObject, string pDeckTextGameObject, string pCP1ScoreGameObject, string pPlayerName)
+    //{
+    //    //gets base parent class info
+    //    base.InitializeObjects(pScoreGameObject, pRoundGameObject, pHandGameObject, pRegionGameObject, pConditionGameObject,
+    //    pPlantGameObject, pInvertebrateGameObject, pAnimalGameObject, pSpecialRegionGameObject, pMultiplayerGameObject,
+    //    pMicrobeGameObject, pFungiGameObject, pDiscardGameObject, pDeckColorGameObject, pDeckTextGameObject,
+    //    pCP1ScoreGameObject, pPlayerName);
+
+    //    CurrentPlayer = this;
+    //    CanDraw = true;
+    //    CardDiscarded = false;
+    //}
+
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    Passive(); 
+    //}
+
+    //public void Passive()
+    //{
+    //    for (int i = 0; i < CurrentPlayer.Deck.Cards.Count; i++)
+    //    {
+    //        switch (CurrentPlayer.Deck.Cards[i].CardName)
+    //        {
+    //            case "Animal-Barred-Owl":
+    //                if (r246())
+    //                {
+    //                    MoveCard(i, AnimalGameObject, Deck.Cards, AnimalPlacement, false);
+    //                }
+    //                //Debug.Log("Barred Owl was found");
+    //                break;
+
+    //        }
+
+    //    }
+    //}
+
+    //public bool CanDraw { get => canDraw; set => canDraw = value; }
+    //public bool CardDiscarded { get => cardDiscarded; set => cardDiscarded = value; }
+    //public passiveaffects CurrentPlayer { get => currentPlayer; set => currentPlayer = value; }
+  
+}
